b4dc62f2f903e1a122d7006cc244f9b5a3508913
tainkrios <tain.krios@gmail.com>
2023-05-31 17:48:35 +0200

    cors

--- index.js
+++ index.js
@@ -5,10 +5,7 @@ const app = express()
 const apiKey = process.env.GOOGLE_API_KEY
 
 app.use((req, res, next) => {
-  res.setHeader(
-    'Access-Control-Allow-Origin',
-    'https://handbrewmap-rg5q87tls-tainkrios.vercel.app/'
-  )
+  res.setHeader('Access-Control-Allow-Origin', '*')
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
   next()
